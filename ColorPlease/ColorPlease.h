2ef6e2310fdddf3ddcde084651626b1ceb4d8043
Daniel Hammond <danielrhammond@gmail.com>
2014-07-13 16:42:22 -0700

    Init

---
+++ ColorPlease/ColorPlease.h
@@ -0,0 +1,19 @@
+//
+//  ColorPlease.h
+//  ColorPlease
+//
+//  Created by Daniel Hammond on 7/13/14.
+//  Copyright (c) 2014 Daniel Hammond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ColorPlease.
+FOUNDATION_EXPORT double ColorPleaseVersionNumber;
+
+//! Project version string for ColorPlease.
+FOUNDATION_EXPORT const unsigned char ColorPleaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorPlease/PublicHeader.h>
+
+
---
+++ ColorPlease/ColorPlease.swift
@@ -0,0 +1,13 @@
+//
+//  ColorPlease.swift
+//  ColorPlease
+//
+//  Created by Daniel Hammond on 7/13/14.
+//  Copyright (c) 2014 Daniel Hammond. All rights reserved.
+//
+
+import UIKit
+
+class ColorPlease: NSObject {
+   
+}
