3a93060857c7857d7148180af79cae2cda9f253d
Daniel Hammond <danielrhammond@gmail.com>
2014-07-13 21:09:14 -0700

    Initial implementation of golden ratio color generator

--- ColorPlease/ColorPlease.swift
+++ ColorPlease/ColorPlease.swift
@@ -8,6 +8,36 @@
 
 import UIKit
 
-class ColorPlease: NSObject {
-   
+let GOLDEN_RATIO_CONJUGATE:Float = 0.618033988749895
+
+class ColorPlease {
+    func randomFloat() -> Float {
+        return Float(arc4random_uniform(100)) / 100.0;
+    }
+    
+    func createColor(golden:Bool = true, baseColor:UIColor?) -> UIColor {
+        var h: CGFloat = 0, s: CGFloat=0, b: CGFloat=0, a: CGFloat=0
+        if let color = baseColor {
+            color.getHue(&h, saturation: &s, brightness: &b, alpha: &a)
+            return createColor(golden: golden, hue: h, saturation: s, brightness: b, alpha: a)
+        } else {
+            return createColor(golden: golden)
+        }
+    }
+    
+    func createColor(golden:Bool = true, hue:Float? = nil, saturation:Float = 0.4, brightness:Float = 0.75, alpha:Float = 1.0) -> UIColor {
+        var seed_hue = hue ? hue! : randomFloat()
+        var h = golden ? fmodf(seed_hue + seed_hue / GOLDEN_RATIO_CONJUGATE, 1.0) : seed_hue
+        return UIColor(hue: h, saturation: saturation, brightness: brightness, alpha: alpha)
+    }
+    
+    func createColorScheme(golden:Bool = true, count:Int) -> [UIColor] {
+        var colors = [UIColor]()
+        var color: UIColor?
+        do {
+            color = createColor(golden:golden, baseColor:color)
+            colors.append(color!)
+        } while colors.count < count
+        return colors
+    }
 }
--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import ColorPlease
 
 class ViewController: UIViewController {
-                            
+
+    let colorGenerator = ColorPlease()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tap = UITapGestureRecognizer(target: self, action: "tapAction")
+        self.view.addGestureRecognizer(tap)
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func tapAction() {
+        for view in self.view.subviews {
+            view.removeFromSuperview()
+        }
+        let colors = colorGenerator.createColorScheme(count:6)
+        let swatchHeight: CGFloat = CGRectGetHeight(self.view.bounds) / Float(colors.count)
+        for (i, color) in enumerate(colors) {
+            let swatch = UIView()
+            swatch.backgroundColor = color
+            let yOrigin: CGFloat = swatchHeight * Float(i)
+            swatch.frame = CGRectMake(CGRectGetMinX(self.view.bounds), yOrigin, CGRectGetWidth(self.view.bounds), swatchHeight)
+            self.view.addSubview(swatch)
+        }
     }
 
     override func didReceiveMemoryWarning() {
